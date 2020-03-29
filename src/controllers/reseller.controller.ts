b81e725131e4a668067030e3f8aa4e97607ae7e9
Jorge Cristian Fernandez <jcfernandez@jcfernandez.local>
2020-03-29 18:14:07 -0300

    ticket_controller_added

--- src/controllers/reseller.controller.ts
+++ src/controllers/reseller.controller.ts
@@ -1,13 +1,7 @@
-import fs from 'fs';
 import jwt from 'jsonwebtoken';
-import QRGenerator from 'qrcode';
-import { v4 as uuidv4 } from 'uuid';
-import handlebars from 'handlebars';
 import moment, { Moment } from 'moment';
-import { QRCode } from '../libs/qr.code';
 import { Response, Request } from "express";
 import { Reset } from '../entities/reset.entity';
-import { Ticket } from '../entities/ticket.entity';
 import { Verify } from '../entities/verify.entity';
 import { BaseController } from "./base.controller";
 import { EntityManager, Connection } from "typeorm";
@@ -362,67 +356,6 @@ export class ResellerController extends BaseController {
 	@Delete('delete/:email')
 	public async delete(request: Request, response: Response) {
 
-	}
-
-	@Post('generateqr')
-	@Middleware([session])
-	public async generateqr(request: Request, response: Response) {
-		const self = this;
-		const { email, opening, reseller } = request.body;
-		const expire = new Date(moment().add(2, 'weeks').format());
-		let code = new QRCode().generate(email, expire);
-
-		let ticket: Ticket = new Ticket();
-
-		ticket.code = code;
-		ticket.owner = email;
-		ticket.used = false;
-		ticket.createdBy = reseller;
-		ticket.opening = opening;
-		ticket.expire = expire;
-
-		let fileLocation = './uploads/' + ticket.owner;
-		let fileName = uuidv4() + '.jpeg';
-		ticket.fullPath = "uploads/" + ticket.owner + '/' + fileName;
-
-		if (!fs.existsSync(fileLocation)) {
-			fs.mkdirSync(fileLocation);
-		}
-
-		const connection: Connection = await self.getConnection();
-		const entityManager = self.getManager();
-
-		const saved = await entityManager.save(Ticket, ticket);
-
-		if (!saved) {
-			connection.close();
-			return response.status(400).json({ message: "error_saving" });
-		}
-
-		await QRGenerator.toFile(fileLocation + '/' + fileName, code, { errorCorrectionLevel: 'H' });
-
-		let file = fs.readFileSync('dist/templates/template.hbs', 'utf8');
-
-		let template = handlebars.compile(file);
-		let compiled = template({ "qr": saved.fullPath });
-		let mailer: Mailer = new Mailer();
-
-		let options: mailOptions = {
-			from: '"NodeJS" <jcfernandez@jcdeveloper.net>',
-			to: email,
-			subject: 'Tu código QR',
-			html: compiled
-		}
-
-		const sent = await mailer.sendMail(options);
-
-		if (!sent) {
-			connection.close();
-			return response.status(401).json({ message: "could_not_send_email" });
-		}
-
-		connection.close();
-		return response.status(200).json({ message: "ticket has been created" });
 	}
 
 }
--- src/controllers/ticket.controller.ts
+++ src/controllers/ticket.controller.ts
@@ -1,8 +1,17 @@
 import { BaseController } from "./base.controller";
-import { Controller, Post } from "@overnightjs/core";
+import { Controller, Post, Middleware } from "@overnightjs/core";
 import { Response, Request } from "express";
 import { Connection, EntityManager } from "typeorm";
 import { Ticket } from "../entities/ticket.entity";
+import moment from "moment";
+import { session } from "../middlewares/session.middleware";
+import { QRCode } from "../libs/qr.code";
+import { Mailer, mailOptions } from "../libs/mailer";
+import { v4 as uuidv4 } from 'uuid';
+import fs from 'fs';
+import QRGenerator from 'qrcode';
+import handlebars from 'handlebars';
+
 
 @Controller('api/ticket')
 export class TicketController extends BaseController {
@@ -22,7 +31,64 @@ export class TicketController extends BaseController {
 	}
 
 	@Post('create')
+	@Middleware([session])
 	public async create(request: Request, response: Response) {
-		return response.status(200).json({ message: "ok" })
+		const self = this;
+		const { email, opening, reseller } = request.body;
+		const expire = new Date(moment().add(2, 'weeks').format());
+		let code = new QRCode().generate(email, expire);
+
+		let ticket: Ticket = new Ticket();
+
+		ticket.code = code;
+		ticket.owner = email;
+		ticket.used = false;
+		ticket.createdBy = reseller;
+		ticket.opening = opening;
+		ticket.expire = expire;
+
+		let fileLocation = './uploads/' + ticket.owner;
+		let fileName = uuidv4() + '.jpeg';
+		ticket.fullPath = "uploads/" + ticket.owner + '/' + fileName;
+
+		if (!fs.existsSync(fileLocation)) {
+			fs.mkdirSync(fileLocation);
+		}
+
+		const connection: Connection = await self.getConnection();
+		const entityManager = self.getManager();
+
+		const saved = await entityManager.save(Ticket, ticket);
+
+		if (!saved) {
+			connection.close();
+			return response.status(400).json({ message: "error_saving" });
+		}
+
+		await QRGenerator.toFile(fileLocation + '/' + fileName, code, { errorCorrectionLevel: 'H' });
+
+		let file = fs.readFileSync('dist/templates/template.hbs', 'utf8');
+
+		let template = handlebars.compile(file);
+		let compiled = template({ "qr": saved.fullPath });
+		let mailer: Mailer = new Mailer();
+
+		let options: mailOptions = {
+			from: '"NodeJS" <jcfernandez@jcdeveloper.net>',
+			to: email,
+			subject: 'Tu código QR',
+			html: compiled
+		}
+
+		const sent = await mailer.sendMail(options);
+
+		if (!sent) {
+			connection.close();
+			return response.status(401).json({ message: "could_not_send_email" });
+		}
+
+		connection.close();
+		return response.status(200).json({ message: "ticket has been created" });
 	}
+
 }
