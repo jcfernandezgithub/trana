62881bcea9b75647413b2069e7e4f53bb052ec47
Jorge Cristian Fernandez <jcfernandez@jcfernandez.local>
2020-04-01 12:44:30 -0300

    super_call_reseller_workaround_revert

--- src/controllers/base.controller.ts
+++ src/controllers/base.controller.ts
@@ -2,19 +2,5 @@ import { Database } from '../database/database.class';
 import { Connection, EntityManager } from 'typeorm';
 
 export class BaseController extends Database {
-	public connection: Connection;
-	public entityManager: EntityManager;
 
-	constructor() {
-		super();
-		this.makeConnection();
-
-		this.entityManager = this.getManager();
-	}
-
-	private async makeConnection(): Promise<void> {
-		const self = this;
-		const connection = await self.getConnection();
-		self.connection = connection;
-	}
 }
