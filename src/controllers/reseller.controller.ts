97b1715b83d6d1c069eeb3d32f4744acbfdb0578
Jorge Cristian Fernandez <jcfernandez@jcfernandez.local>
2020-04-02 13:28:15 -0300

    fixes_reseller_reset_password

--- src/controllers/reseller.controller.ts
+++ src/controllers/reseller.controller.ts
@@ -192,24 +192,24 @@ export class ResellerController extends BaseController {
 		return response.status(200).json({ "message": "updated" });
 	}
 
-	@Get('forgot/:email')
+	@Get('forgot/:id')
 	public async forgot(request: Request, response: Response) {
 		const self = this;
-		const email = request.params.email;
+		const id: ObjectId = new ObjectId(request.params.id);
 		const connection = await self.getConnection();
 		const entityManager: EntityManager = self.getManager();
-		const reseller = await entityManager.findOneOrFail(Reseller, { email: email });
+		const reseller: Reseller | undefined = await entityManager.findOne(Reseller, { _id: id });
 
 		if (!reseller) {
 			let res = {
 				success: false,
 				message: "Reseller not found"
 			}
-
+			connection.close();
 			return response.status(400).json(res);
 		}
 
-		await entityManager.delete(Reset, { email: email });
+		await entityManager.delete(Reset, { _id: id });
 
 		let reset: Reset = new Reset();
 		const token: string = await reset.createToken();
@@ -229,7 +229,7 @@ export class ResellerController extends BaseController {
 			return response.status(400).json(result);
 		}
 
-		const url = `${request.protocol}://${request.get('host')}/api/reseller/reset/${email}/${encodeURIComponent(token.toString())}`;
+		const url = `${request.protocol}://${request.get('host')}/api/reseller/reset/${id}/${encodeURIComponent(token.toString())}`;
 
 		let options: mailOptions = {
 			from: '"NodeJS" <jcfernandez@jcdeveloper.net>',
@@ -245,7 +245,7 @@ export class ResellerController extends BaseController {
 		const sent = mailer.sendMail(options);
 
 		if (!sent) {
-			console.log(email);
+			console.log(id);
 			let res = {
 				success: false,
 				message: "We couldn't process your request, try again"
@@ -262,21 +262,23 @@ export class ResellerController extends BaseController {
 		return response.status(200).json(res);
 	}
 
-	@Get('reset/:email/:token')
+	@Get('reset/:id/:token')
 	public async reset(request: Request, response: Response) {
-		const email = request.params.email;
+		const id = request.params.id;
 		const token = decodeURIComponent(request.params.token);
 
-		return response.render('reset', { email, token });
+		return response.render('reset', { id, token });
 	}
 
 	@Post('password')
 	public async password(request: Request, response: Response) {
 		const self = this;
-		const { email, token, password } = request.body;
+		const id: ObjectId = new ObjectId(request.body.id);
+		const token = request.body.token;
+		const password = request.body.password;
 		const connection: Connection = await self.getConnection();
 		const entityManager: EntityManager = self.getManager();
-		const reset: Reset | undefined = await entityManager.findOne(Reset, { email: email });
+		const reset: Reset | undefined = await entityManager.findOne(Reset, { _id: id });
 
 		if (reset === undefined) {
 			let res = {
@@ -311,13 +313,13 @@ export class ResellerController extends BaseController {
 		}
 
 
-		const filter = { email: email };
+		const filter = { _id: id };
 		const reseller = new Reseller();
 		const update = { password: await reseller.encrypt(password) };
 		const doc = await entityManager.update(Reseller, filter, update);
 
 		if (doc) {
-			await entityManager.delete(Reset, { email: email });
+			await entityManager.delete(Reset, { _id: id });
 		}
 
 		let res = {
