fa366c46f135fddcd1c481f761ab52fcc31cda29
Jorge Cristian Fernandez <jcfernandez@jcfernandez.local>
2020-04-06 14:59:05 -0300

    socket_first_steps

--- src/app.ts
+++ src/app.ts
@@ -58,7 +58,7 @@ export default class App extends Server {
 
 
 			socket.on('ticket_delete', (message) => {
-				socket.broadcast.to(socket.id).emit('callback', { message: "hola" })
+				io.emit('callback', { message: "hola" })
 			});
 
 		});
