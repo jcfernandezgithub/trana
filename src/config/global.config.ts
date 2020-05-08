ad2fd0db384485ba5b3b2abfbb86c88e8fd48b32
Jorge Cristian Fernandez <jcfernandez@jcfernandez.local>
2020-05-07 22:33:29 -0300

    dotenv_target

--- src/config/global.config.ts
+++ src/config/global.config.ts
@@ -4,7 +4,7 @@ dotenv.config();
 export default {
 	PORT: process.env.PORT,
 	ENVIRONMENT: process.env.ENVIRONMENT,
-	JWT_KEY: process.env.JWT_KEY,
+	JWT_KEY: '6c650fa9285c2c0eaf2027a5acd33a3c21b5d6299560b53b84ff1c545d2841461b75060a2a265de1cd5774c7f5ba4b47c7b2e6873c6f89a6870c1edd79853c72',
 	MAILER: {
 		host: 'smtp.hostinger.com.ar',
 		port: 587,
